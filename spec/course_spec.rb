65bf1daf291f00b015c0e6ea12535853ab08a578
Nick Sacco <nicksacco10@gmail.com>
2023-02-15 09:02:19 -0800

    Test: enroll students

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -22,5 +22,16 @@ RSpec.describe Course do
     expect(@course.full?).to eq(false)
   end
 
+  it 'can enroll students' do
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+
+    @course.enroll(student1)
+    @course.enroll(student2)
+
+    expect(@course.students).to eq([student1, student2])
+
+
+  end
 
 end
