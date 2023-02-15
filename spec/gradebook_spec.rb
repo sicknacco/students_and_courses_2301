66069a359010b16350b20bfe3196dca0d8eb1ef7
Nick Sacco <nicksacco10@gmail.com>
2023-02-15 09:33:28 -0800

    Test: Add instructor test

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe Gradebook do
     expect(@gradebook).to be_instance_of(Gradebook)
     end
 
+    it 'has an instructor' do
+      expect(@gradebook.instructor).to eq('Jimmy')
+    end
+    
     it 'starts with no courses' do
       expect(@gradebook.courses).to eq([])
     end
