3fe98ae158c1590c07ebc5dddd5fee214f3a8869
Nick Sacco <nicksacco10@gmail.com>
2023-02-15 08:07:52 -0800

    Test: Initialize test

---
+++ spec/student_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/student'
+
+RSpec.describe Student do
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
+
+  it 'exists' do
+    expect(@student).to be_instance_of(Student)
+  end
+end
